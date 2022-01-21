d1b51de1825078d53b5da0e461d65d84fa4c2e85
kazutakaohki <95158613+kazutakaohki@users.noreply.github.com>
2022-01-21 17:51:14 +0900

    Add files via upload

---
+++ php02_kadai/insert.php
@@ -0,0 +1,52 @@
+<?php
+require_once("funcs.php");
+
+
+/**
+ * 1. index.phpのフォームの部分がおかしいので、ここを書き換えて、
+ * insert.phpにPOSTでデータが飛ぶようにしてください。
+ * 2. insert.phpで値を受け取ってください。
+ * 3. 受け取ったデータをバインド変数に与えてください。
+ * 4. index.phpフォームに書き込み、送信を行ってみて、実際にPhpMyAdminを確認してみてください！
+ */
+
+//1. POSTデータ取得
+
+$name = $_POST["name"];
+$address = $_POST["address"];
+$tel = $_POST["tel"];
+$category = $_POST["category"];
+$star = $_POST["star"];
+$comment = $_POST["comment"];
+$date = $_POST["date"];
+
+
+//2. DB接続します
+$pdo = db_conn();
+
+//３．データ登録SQL作成
+// 1. SQL文を用意
+$stmt = $pdo->prepare("INSERT INTO st_spot_table(id, name, address, tel, category, star, comment, date)
+                        VALUES(NULL, :name, :address, :tel, :category, :star, :comment, sysdate())");
+//  2. バインド変数を用意
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':address', $address, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':tel', $tel, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':category', $category, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':star', $star, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':comment', $comment, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+
+//  3. 実行
+$status = $stmt->execute();
+
+//４．データ登録処理後
+if($status==false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit("ErrorMessage:".$error[2]);
+}else{
+  //５．index.phpへリダイレクト
+header("Location: index.php");
+
+}
+?>
---
+++ php02_kadai/select.php
@@ -0,0 +1,72 @@
+
+<?php
+require_once("funcs.php");
+
+//1.  DB接続します
+$pdo = db_conn();
+
+//２．データ取得SQL作成
+$stmt = $pdo->prepare("SELECT * FROM st_spot_table");
+$status = $stmt->execute();
+
+//３．データ表示
+$view = "";
+if ($status == false) {
+    //execute（SQL実行時にエラーがある場合）
+    $error = $stmt->errorInfo();
+    exit("ErrorQuery:" . $error[2]);
+} else {
+    //Selectデータの数だけ自動でループしてくれる
+    //FETCH_ASSOC=http://php.net/manual/ja/pdostatement.fetch.php
+    while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $view .= '<p>';
+        $view .= h($result['id']) . '/' . h($result['name']) . '/' . h($result['address']) . '/' . h($result['tel']);
+        $view .= h($result['category']) . '/' . h($result['star']) . '/' . h($result['comment'] . '/' . h($result['date']));
+        $view .= '</p>';
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>登録スポット一覧</title>
+    <link rel="stylesheet" href="css/range.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        div {
+            padding: 10px;
+            font-size: 16px;
+        }
+    </style>
+</head>
+
+<body id="main">
+    <!-- Head[Start] -->
+    <header>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index.php">データ登録</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!-- Head[End] -->
+
+    <!-- Main[Start] -->
+    <div>
+        <div class="container jumbotron"><?= $view ?></div>
+    </div>
+    <!-- Main[End] -->
+</body>
+
+</html>
+
+
+
